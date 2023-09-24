4c2c37d610a3d6b8f90a1000816e41d28b97ded5
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-24 20:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+
+  // Get the size of the Blob in bytes
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
